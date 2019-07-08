5aadf62626c59730920e555926f80ecc3c50b79e
Mikhail Novikov <mnovikov@mera.ru>
2019-07-08 18:36:11 +0300

    added caorusel

--- main.js
+++ main.js
@@ -29,4 +29,66 @@ window.onload = function() {
     document.querySelector('#scrollDown').addEventListener('click', function(){
         document.querySelector('.nav').scrollIntoView({behavior : 'smooth'});
     })
+
+    var imagesUrl = [
+        'assets/wrapper.jpg',
+        'assets/wrapper.jpg',
+        'assets/wrapper.jpg',
+        'assets/wrapper.jpg'
+    ]
+    var caorusel = new Caorusel();
+    caorusel.init('caorusel', imagesUrl);
+}
+
+function Caorusel() {
+    this.body = null;
+    this.imageNodes = null;
+    this.left = null;
+    this.right = null;
+    this.activeNode = null;
+}
+
+Caorusel.prototype = {
+    init : function(rootID, imagesUrl) {
+        //init body of the caorusel
+        var root = document.getElementById(rootID);
+        root.className = 'caorusel';
+        this.body = root;
+
+        //init imagesNodes
+        this.activeNode = imagesUrl.length / 2 | 0;
+        this.imageNodes = imagesUrl.map(function(imageUrl) {
+            var imageNode = document.createElement('img');
+            imageNode.className = 'caorusel__image';
+            imageNode.setAttribute('src', imageUrl);
+            imageNode.classList.add('caorusel__image_inactive');
+            return imageNode
+        })
+        for (var i = 0; i < this.imageNodes.length; i++) {
+            this.body.appendChild(this.imageNodes[i]);
+        }
+
+        //init arrows    
+        var f = this.activate.bind(this); 
+        f(this.activeNode);
+        setTimeout(function() {
+            f(1)
+        }, 3000)
+    },
+    activate : function(index) {
+        var activeNode = this.imageNodes[index];
+        var currentOffset = activeNode.offsetLeft + activeNode.offsetWidth / 2;
+        var targetOffset = document.body.offsetWidth / 2;
+
+        var newMargin = targetOffset - currentOffset;
+        if (newMargin > 0) {
+            while (currentOffset <= targetOffset) {
+                this.body.style['margin-left'] = Number.parseInt(this.body.style['margin-left']) + 1 + 'px';
+                currentOffset = activeNode.offsetLeft + activeNode.offsetWidth / 2;
+            };
+        }
+        this.body.style['margin-left'] = newMargin + 'px';
+        activeNode.classList.remove('caorusel__image_inactive');
+        // this.activeNode.classList.add('caorusel__image_inactive');
+    }
 }
